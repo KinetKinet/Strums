0cc45a1a66c3d379ceb4ff59917fc32f477cd63f
Kenneth Bawiin <kennethjaebawiin@gmail.com>
2025-12-06 02:29:05 +0800

    s

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
-// Check for saved theme preference
+// check for saved theme preference
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
     body.classList.add('dark-mode');
     //    toggleButton.textContent = 'Light Mode';
 }
 
-   // Toggle function
+   // toggle function
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('theme-toggle');
     if (toggleButton) {
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const isDark = body.classList.contains('dark-mode');
             toggleButton.textContent = isDark ? 'Light Mode' : 'Dark Mode';
             
-            // Save preference
+            // save preference
             localStorage.setItem('theme', isDark ? 'dark' : 'light');
         });
      }
