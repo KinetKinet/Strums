7b445a3d5401d9237386740f2ea248d365fac633
Kenneth Bawiin <kennethjaebawiin@gmail.com>
2025-12-06 02:48:35 +0800

    fixed fouc

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,24 @@
 const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
 
 // check for saved theme preference
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
-    body.classList.add('dark-mode');
-    //    toggleButton.textContent = 'Light Mode';
+    document.documentElement.classList.add('dark-mode');
 }
 
-   // toggle function
+// toggle function
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('theme-toggle');
     if (toggleButton) {
-        toggleButton.textContent = body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+        toggleButton.textContent = document.documentElement.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
 
         toggleButton.addEventListener('click', () => {
-            body.classList.toggle('dark-mode');
-            const isDark = body.classList.contains('dark-mode');
+            document.documentElement.classList.toggle('dark-mode');
+            const isDark = document.documentElement.classList.contains('dark-mode');
             toggleButton.textContent = isDark ? 'Light Mode' : 'Dark Mode';
             
             // save preference
             localStorage.setItem('theme', isDark ? 'dark' : 'light');
         });
-     }
+    }
 });
-   
